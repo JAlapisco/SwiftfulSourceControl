21ea453cb6a6106e9c484f94136e7cf7d99de4f7
jalapisco <jorge_alapisco@cox.net>
2024-08-29 20:56:13 -0700

    Initial Commit

---
+++ SwiftfulSourceControl/SwiftfulSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulSourceControlApp.swift
+//  SwiftfulSourceControl
+//
+//  Created by Jesus Jorge Alapisco on 8/29/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
