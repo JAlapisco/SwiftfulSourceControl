25588d8427cc7dfef63c5b7b90eb88275ab1b3ec
jalapisco <jorge_alapisco@cox.net>
2024-09-10 20:24:50 -0700

    Add HomeView

---
+++ SwiftfulSourceControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Jesus Jorge Alapisco on 9/10/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Screen 2")
+    }
+}
+
+#Preview {
+    HomeView()
+}
