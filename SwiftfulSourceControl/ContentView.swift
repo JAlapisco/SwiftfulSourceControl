c3d79877d0c45d91b6731f6fddfa239e71afb1a6
jalapisco <jorge_alapisco@cox.net>
2024-09-14 20:53:45 -0700

    [Clean] Updates to documentation

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -5,6 +5,34 @@
 //  Created by Jesus Jorge Alapisco on 8/29/24.
 //
 
+/*
+ Clone  = Copying the repository locally
+ Commit = Save ("checkpoint") on our current branch
+ Stage  = Prepare changes for a commit
+ Stash  = Save changes for later
+ Push   = Send local commits to remote repo
+ pull   = Fetch remote commits to local repo
+ 
+ COMMIT MESSAGES:
+ 
+ NEW FEATURE:
+ [Feature] Description of the feature
+ 
+ BUG NOT IN PRODUCTION:
+ [Bug] Description of the bug
+ 
+ RELEASE:
+ [Release] Description of release
+ 
+ BUG IN PRODUCTION:
+ [Patch] Description of patch
+ 
+ MUNDANE TASKS:
+ [Clean] Description of changes
+ */
+
+
+
 import SwiftUI
 
 struct ContentView: View {
