2f228ad6943776976cd0f8259aac1a9e81c5b08a
jalapisco <jorge_alapisco@cox.net>
2024-09-18 21:00:33 -0700

    [Clean] Resolve merge conflict

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -39,7 +39,7 @@ struct ContentView: View {
             ScrollView {
                 VStack {
                     ForEach(0..<20) { _ in
-                        Image(systemName: "globle")
+                        Image(systemName: "globe")
                             .font(.largeTitle)
                             .imageScale(.large)
                             .foregroundStyle(.tint)
