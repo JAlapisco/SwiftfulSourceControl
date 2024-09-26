9e5feaaec3966cc8e7d31e568442363f23eeb5e3
jalapisco <jorge_alapisco@cox.net>
2024-09-25 21:08:20 -0700

    [Feature] Add SettingsView

---
+++ SwiftfulSourceControl/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Jesus Jorge Alapisco on 9/25/24.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    SettingsView()
+}
