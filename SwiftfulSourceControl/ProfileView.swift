3c59ab1929359583c60a4df502fa93f1c62b33e5
jalapisco <jorge_alapisco@cox.net>
2024-10-15 21:29:54 -0700

    [Feature] Add ProfileView

---
+++ SwiftfulSourceControl/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Jesus Jorge Alapisco on 10/15/24.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Jorge")
+    }
+}
+
+#Preview {
+    ProfileView()
+}
